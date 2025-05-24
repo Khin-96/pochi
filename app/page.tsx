b11aeee6c3096c2adfb9322a699d4450164dc4f1
Khin <hazmonkinga@gmail.com>
2025-05-24 13:22:38 +0300

    updated image location

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function LandingPage() {
             </div>
             <div className="flex justify-center lg:justify-end">
               <Image
-                src="/placeholder.svg.png?height=500&width=500"
+                src="/placeholder.png?height=500&width=500"
                 alt="PochiYangu App Screenshot"
                 width={500}
                 height={500}
